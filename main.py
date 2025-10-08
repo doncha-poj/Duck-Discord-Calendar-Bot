353e4e062ae299e6ea6bf4373704283cb0aca0c3
doncha-poj <100371989+dchanthirath@users.noreply.github.com>
2025-10-08 15:29:03 -0500

    cleaned up commented out code

--- main.py
+++ main.py
@@ -18,42 +18,18 @@ intents.members = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-# @bot.event
-# async def on_ready():
-#     print(f"We are ready to go in, {bot.user.name}")
-
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=guild_id))
     print(f"We are ready to go in, {client.user.name}")
 
-# @bot.event
-# async def on_member_join(member):
-#     await member.send(f"Welcome to the server {member.name}")
-
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-#
-#     if "shit" in message.content.lower():
-#         await message.delete()
-#         await message.channel.send(f"{message.author.mention} - don't use that word")
-#
-#     await bot.process_commands(message)
-
-# /hello
-# @bot.command()
-# async  def hello(ctx):
-#     await ctx.send(f"Hello {ctx.author.mention}!")
 
 @tree.command(
     name="hello",
     description="Say hi",
     guild=discord.Object(id=guild_id)
 )
-async def first_command(interaction):
+async def test_command(interaction):
     await interaction.response.send_message(f"hi, {interaction.user.mention}")
 
 client.run(token, log_handler=handler, log_level=logging.DEBUG)
-# bot.run(token, log_handler=handler, log_level=logging.DEBUG)
