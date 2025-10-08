cab8ebf7b196ecedd4c02391dcd301b85e8e501c
doncha-poj <100371989+dchanthirath@users.noreply.github.com>
2025-10-08 15:47:47 -0500

    added setup for timezone information

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ from discord import app_commands
 import logging
 from dotenv import load_dotenv
 import os
+from datetime import time
+from zoneinfo import ZoneInfo
 
+# Setup Config
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 guild_id = os.getenv('GUILD_ID')
+POLL_CHANNEL_ID = int(os.getenv('POLL_CHANNEL_ID', 0))
+EASTERN = ZoneInfo("America/New_York")
 
+# Logging
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+# Initialization
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
@@ -17,10 +25,16 @@ intents.members = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+# Bot Events
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=guild_id))
     print(f"We are ready to go in, {client.user.name}")
+
+    # Start any tasks
+    if not daily_poll.is_running():
+        daily_poll.start()
+        print("Daily poll task has been started.")
 
 
 @tree.command(
@@ -28,7 +42,10 @@ async def on_ready():
     description="Say hi",
     guild=discord.Object(id=guild_id)
 )
+
+# Test Command
 async def test_command(interaction):
     await interaction.response.send_message(f"hi, {interaction.user.mention}")
 
+# Running
 client.run(token, log_handler=handler, log_level=logging.DEBUG)
